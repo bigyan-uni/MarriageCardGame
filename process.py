3749d6594e5f87c20626d770a9ee9d6c3adf4d80
Bigyan Acharya <2achab31@solent.ac.uk>
2024-01-12 17:04:19 +0000

    Finished writing code to allow user to input their hand into the program. Invalid hands are not allowed and code iterates until hand is full of valid cards.

--- process.py
+++ process.py
@@ -1,35 +1,65 @@
-cards = {"A1": 4, "A2": 4, "A3": 4, "A4": 4, "A5": 4, "A6": 4, "A7": 4, "A8": 4, "A9": 4, "A10": 4, "A11": 4, "A12": 4,
-         "A13": 4, "B1": 4, "B2": 4, "B3": 4, "B4": 4, "B5": 4, "B6": 4, "B7": 4, "B8": 4, "B9": 4, "B10": 4, "B11": 4,
-         "B12": 4, "B13": 4, "C1": 4, "C2": 4, "C3": 4, "C4": 4, "C5": 4, "C6": 4, "C7": 4, "C8": 4, "C9": 4, "C10": 4,
-         "C11": 4, "C12": 4, "C13": 4, "D1": 4, "D2": 4, "D3": 4, "D4": 4, "D5": 4, "D6": 4, "D7": 4, "D8": 4, "D9": 4,
-         "D10": 4, "D11": 4, "D12": 4, "D13": 4}
+import tui
 
 
-# Function that standardizes the card to suit "A,B,C,D"
+# Function that standardizes the card to suit "H,S,C,D"
 def standard(card: str) -> str:
     card = card.upper().strip()
-    card.replace("HEART", "A")
-    card.replace("H", "A")
-    card.replace("SPADE", "B")
-    card.replace("S", "B")
-    card.replace("CLUB", "C")
-    # card.replace("C", "C")
-    card.replace("DIAMOND", "D")
-    # card.replace("D", "D")
-    card.replace("JACK", "11")
-    card.replace("J", "11")
-    card.replace("QUEEN", "12")
-    card.replace("Q", "D")
-    card.replace("KING", "13")
-    card.replace("K", "13")
+    card = card.replace("HEART", "H")
+    card = card.replace("SPADE", "S")
+    card = card.replace("CLUS", "C")
+    card = card.replace("DIAMOND", "D")
+    card = card.replace("JACK", "11")
+    card = card.replace("J", "11")
+    card = card.replace("QUEEN", "12")
+    card = card.replace("Q", "12")
+    card = card.replace("KING", "13")
+    card = card.replace("K", "13")
+    card = card.replace("A", "1")
     return card
 
 
+# Function that checks if input card is valid
+def valid(card: str) -> bool:
+    num = card[:-1]
+    suit = card[-1:]
+    is_valid = False
+    if num in ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13"]:
+        if suit in ["H", "S", "C", "D"]:
+            is_valid = True
+        else:
+            tui.error("Invalid Suit.")
+    else:
+        tui.error("Invalid number.")
+    return is_valid
+
+
 # Function to return number of specific card. Returns int.
-def card_num(card: str) -> int:
-    return cards[card]
+def card_num(card: str, cards: dict) -> int:
+    num = cards[card]
+    return num
 
 
 # Function that decrements number of specific card. Returns nothing.
-def discard(card: str):
-    cards[card] -= 1
+def discard(card: str, cards: dict):
+    if card_num(card, cards) > 0:
+        cards[card] -= 1
+
+
+# Function that increments number of specific card. Returns nothing.
+def draw(card: str, hand: dict):
+    hand[card] += 1
+
+
+#  Function to make initial hand during first draw
+def make_hand(hand: dict, cards: dict):
+    i = 1
+    while i <= 21:
+        card = tui.add_card()
+        if valid(card):
+            if card_num(card, cards) > 0:
+                draw(card, hand)  # add card to hand
+                discard(card, cards)  # remove from main deck
+                tui.see_hand(hand)
+                i += 1
+            else:
+                tui.error("Too many cards of same type.")
--- tui.py
+++ tui.py
@@ -1,13 +1,48 @@
-from process import standard
+import process
 
 
+# DISPLAYS TITLE OF PROGRAM
 def title():
-    msg = "|| Welcome to MARRIAGE CARD GAME! ||"
+    msg = "|♥♣♠♦| Welcome to MARRIAGE CARD GAME! |♥♣♠♦|"
     print("~" * len(msg))
     print(msg)
     print("~" * len(msg))
 
 
+# TAKES USER INPUT CARD AND RETURNS STANDARDISED INPUT
 def add_card():
+    print("Number + H♥ C♣ S♠ D♦")
     card = input("ADD card to hand: ")
-    return standard(card)
+    return process.standard(card)
+
+
+# DISPLAYS CARDS IN HAND
+def see_hand(hand: dict):
+    for card, num in hand.items():
+        if num != 0:
+            number = card[:-1]
+            suit = card[-1:]
+            display = ""
+            if number == "11":
+                number = "J"
+            elif number == "12":
+                number = "Q"
+            elif number == "13":
+                number = "K"
+            elif number == "1":
+                number = "A"
+            if suit == "H":
+                display = number + "♥"
+            elif suit == "C":
+                display = number + "♣"
+            elif suit == "S":
+                display = number + "♠"
+            elif suit == "D":
+                display = number + "♦"
+            print(f"[{display}], " * num, end="")
+    print("\n")
+
+
+# Displays Error Message
+def error(msg: str):
+    print(f"Error! {msg}")
